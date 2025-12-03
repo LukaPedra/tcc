4ac990ddb4189c568f8d3c715b57adcf33cf9541
Lucca Rocha <lucca.v.rocha@gmail.com>
2025-12-03 09:12:49 -0300

    working training

---
+++ src/main.py
@@ -0,0 +1,86 @@
+import os
+import torch
+import gym
+import gym_super_mario_bros
+from nes_py.wrappers import JoypadSpace
+from gym.wrappers import FrameStack
+import datetime
+from pathlib import Path
+
+# Local imports
+from util import SkipFrame, GrayScaleObservation, ResizeObservation
+from agent import Mario
+from logging_local import MetricLogger
+
+# Initialize Environment
+if gym.__version__ < '0.26':
+    env = gym_super_mario_bros.make("SuperMarioBros-1-1-v0", new_step_api=True)
+else:
+    env = gym_super_mario_bros.make("SuperMarioBros-1-1-v0", render_mode='rgb', apply_api_compatibility=True)
+
+# Limit the action-space to
+#   0. walk right
+#   1. jump right
+env = JoypadSpace(env, [["right"], ["right", "A"]])
+
+# Apply Wrappers to environment
+env = SkipFrame(env, skip=4)
+env = GrayScaleObservation(env)
+env = ResizeObservation(env, shape=84)
+if gym.__version__ < '0.26':
+    env = FrameStack(env, num_stack=4, new_step_api=True)
+else:
+    env = FrameStack(env, num_stack=4)
+
+# Environment setup checks
+env.reset()
+next_state, reward, done, trunc, info = env.step(action=0)
+print(f"State shape: {next_state.shape},\nReward: {reward},\nDone: {done},\nInfo: {info}")
+
+# Training setup
+use_cuda = torch.cuda.is_available()
+print(f"Using CUDA: {use_cuda}")
+print()
+
+save_dir = Path("checkpoints") / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+save_dir.mkdir(parents=True)
+
+mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir)
+logger = MetricLogger(save_dir)
+
+episodes = 40000
+
+print("Starting training...")
+for e in range(episodes):
+
+    state = env.reset()
+
+    # Play the game!
+    while True:
+
+        # Run agent on the state
+        action = mario.act(state)
+
+        # Agent performs action
+        next_state, reward, done, trunc, info = env.step(action)
+
+        # Remember
+        mario.cache(state, next_state, action, reward, done)
+
+        # Learn
+        q, loss = mario.learn()
+
+        # Logging
+        logger.log_step(reward, loss, q)
+
+        # Update state
+        state = next_state
+
+        # Check if end of game
+        if done or info["flag_get"]:
+            break
+
+    logger.log_episode()
+
+    if (e % 20 == 0) or (e == episodes - 1):
+        logger.record(episode=e, epsilon=mario.exploration_rate, step=mario.curr_step)
